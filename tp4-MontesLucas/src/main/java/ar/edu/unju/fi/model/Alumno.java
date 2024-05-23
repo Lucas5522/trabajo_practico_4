e85a30de8fe89cd85e0447a51f5f78b123d6ffa8
Lucas5522 <luceitormaser@gmail.com>
2024-05-23 18:52:00 -0300

    Creacionde la rama para el dearrolo del tp4
    creacion del controller y model con sus respectivas clases
    creacion de index.html y una porcion de codigo para verificar su
    funcionamiento

---
+++ tp4-MontesLucas/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,107 @@
+package ar.edu.unju.fi.model;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	long dni;
+	String nombre;
+	String apellido;
+	String email;
+	long telefono;
+	LocalDate fechaNacimeinto;
+	String domicilio;
+	int libretaUniversitaria;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Alumno(long dni, String nombre, String apellido, String email, long telefono, LocalDate fechaNacimeinto,
+			String domicilio, int libretaUniversitaria) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.fechaNacimeinto = fechaNacimeinto;
+		this.domicilio = domicilio;
+		this.libretaUniversitaria = libretaUniversitaria;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+				+ ", telefono=" + telefono + ", fechaNacimeinto=" + fechaNacimeinto + ", domicilio=" + domicilio
+				+ ", libretaUniversitaria=" + libretaUniversitaria + "]";
+	}
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFechaNacimeinto() {
+		return fechaNacimeinto;
+	}
+
+	public void setFechaNacimeinto(LocalDate fechaNacimeinto) {
+		this.fechaNacimeinto = fechaNacimeinto;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public int getLibretaUniversitaria() {
+		return libretaUniversitaria;
+	}
+
+	public void setLibretaUniversitaria(int libretaUniversitaria) {
+		this.libretaUniversitaria = libretaUniversitaria;
+	}
+	
+	
+	
+	
+	
+}
